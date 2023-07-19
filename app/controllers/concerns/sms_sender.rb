270e30f2d7553631c8e4c6361476a4986926204a
imhilla <hillaryodhiambo282@gmail.com>
2023-07-19 15:26:43 +0300

    updatesgit push

--- app/controllers/concerns/sms_sender.rb
+++ app/controllers/concerns/sms_sender.rb
@@ -1,3 +1,7 @@
+require "net/http"
+require "uri"
+require "json"
+
 module SmsSender
   extend ActiveSupport::Concern
 
@@ -5,8 +9,39 @@ module SmsSender
     private
 
     def send_sms_provider1(to_number, message)
+      @provider1 = "https://mock-text-provider.parentsquare.com/provider1"
+      @provider2 = "https://mock-text-provider.parentsquare.com/provider2"
+
       # Code to send SMS using Provider 1
-      print("heeeeeee")
+      uri = URI.parse(@provider1)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true if uri.scheme == "https"
+
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.body = { to_number: to_number, message: message, callback_url: "https://abb9-41-80-118-187.ngrok.io/delivery_status" }.to_json
+      request.content_type = "application/json"
+
+      response = http.request(request)
+
+      if response.is_a?(Net::HTTPSuccess)
+        # Successful response
+        body = response.body
+        # Process the response body as needed
+        puts(body, "bodybodybodybodybodybodybodybodybodybody")
+      else
+        # Handle unsuccessful response
+        error_message = "Request failed with code #{response.code}"
+        # Handle the error message appropriately, use second provider
+        uri = URI.parse(@provider2)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true if uri.scheme == "https"
+
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.body = { to_number: to_number, message: message, callback_url: "https://abb9-41-80-118-187.ngrok.io/delivery_status" }.to_json
+        request.content_type = "application/json"
+
+        response = http.request(request)
+      end
     end
 
     def send_sms_provider2(to_number, message)
@@ -15,16 +50,16 @@ module SmsSender
     end
 
     def load_balance(messages, percentage)
-      size = messages.size
-      if size == 1
-        return [messages, []]
-      elsif size == 2
-        return [messages[0], messages[1]]
+      case messages.size
+      when 0
+        [[], []]
+      when 1
+        [messages, []]
+      when 2
+        [messages[0], messages[1]]
       else
-        split_index = (size * percentage).to_i
-        first_part = messages[0...split_index]
-        second_part = messages[split_index..-1]
-        [first_part, second_part]
+        split_index = (messages.size * percentage).to_i
+        messages.partition.with_index { |_, index| index < split_index }
       end
     end
   end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,16 +6,26 @@ class MessagesController < ApplicationController
 
   def create
     # get messages/message from json object
-    messages = params["messages"]
+    messages = Array(params["messages"])
     if messages
-      if messages.size == 0
+      if messages.empty?
         render json: { message: "Please provide a valid phone number and message" }
       else
         # manual load balancing
         percentage = 0.3
         first_part, second_part = load_balance(messages, percentage)
 
-        puts(first_part, "first_partfirst_part")
+        first_part.each do |message|
+          to_number = message["to_number"]
+          content = message["message"]
+          send_sms_provider1(to_number, content)
+          
+        end
+
+        # puts("################################")
+        # puts(first_part, "first_partfirst_part")
+        # puts("################################")
+        # puts(second_part, "second_partsecond_part")
         # send_sms_provider1(to_number, message)
         # print(to_number)
         # print(message)
