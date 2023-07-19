844234881ba693ca6d79e3a7b91c5c0abf42598a
imhilla <hillaryodhiambo282@gmail.com>
2023-07-19 21:09:56 +0300

    updates

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   include SmsSender
 
   def create
-    message_params = params.permit(:to_number, :message, :message_id)
+    message_params = params.permit(:to_number, :message, :message_id, :status)
     to_number = message_params[:to_number]
     message = message_params[:message]
 
@@ -11,15 +11,19 @@ class MessagesController < ApplicationController
       return
     end
 
-    success = send_sms(to_number, message)
-
-    if success["success"]
-      message_id = JSON.parse(success["body"])["message_id"]
-
-      Message.create(to_number: to_number, message: message, message_id: message_id)
-      render json: { message: "SMS sent successfully", to_number: to_number, message_id: message_id }
+    message = Message.find_by(params[:to_number])
+    if message.status == "invalid"
+      render json: { message: "Invalid phone number", to_number: to_number }
     else
-      render_failure_message(to_number)
+      success = send_sms(to_number, message)
+      if success["success"]
+        message_id = JSON.parse(success["body"])["message_id"]
+
+        Message.create(to_number: to_number, message: message, message_id: message_id)
+        render json: { message: "SMS sent successfully", to_number: to_number, message_id: message_id }
+      else
+        render_failure_message(to_number)
+      end
     end
   end
 
