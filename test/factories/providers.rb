d339b788631b1b4a846827a5a1f5ea03ccca4e88
imhilla <hillaryodhiambo282@gmail.com>
2023-07-20 13:30:33 +0300

    updates

--- test/factories/providers.rb
+++ test/factories/providers.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe ProvidersController, type: :controller do
   describe "POST #create" do
     context "with valid parameters" do
-      let(:valid_params) { { provider: { name: "Provider One", message_count: 100, url: "http://example.com" } } }
+      let(:valid_params) { { provider: { name: "Provider One", message_count: 100, url: "https://682b-41-80-118-187.ngrok.io/delivery_status" } } }
 
       it "creates a new provider" do
         expect {
@@ -21,7 +21,7 @@ RSpec.describe ProvidersController, type: :controller do
         parsed_response = JSON.parse(response.body)
         expect(parsed_response["name"]).to eq("Provider One")
         expect(parsed_response["message_count"]).to eq(100)
-        expect(parsed_response["url"]).to eq("http://example.com")
+        expect(parsed_response["url"]).to eq("https://682b-41-80-118-187.ngrok.io/delivery_status")
       end
     end
   end
